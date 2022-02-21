9d802a78cccab8878e3b15b97b47c51d57dd4d58
Rafael Santana <rafael.santana@webjump.com.br>
2022-02-21 14:04:42 -0300

    feat: finish

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,17 @@
-export function Content() {
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps{
+  movies: MovieProps[]
+}
+
+export function Content({movies} :ContentProps ) {
   // Complete aqui
+  return(
+    <div className="movies-list">
+    {movies.map(movie => (
+      <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+    ))}
+  </div>
+  )
 }
